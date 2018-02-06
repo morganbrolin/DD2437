5f1d65f0cd47a5db1fb64695b6beb0c1771071c1
Brolin <morgan.brolin@gmail.com>
2018-02-06 11:55:55 +0100

    Add files via upload

--- encoder.py
+++ encoder.py
@@ -75,17 +75,14 @@ def initialize_weights(n, input_dimension, ):
     #    added bias to outer layer. 
     # =============================================================================
     #W1 = np.matrix(np.random.multivariate_normal(mean, cov,size).T)
-    W1 = np.matrix(np.random.normal(0,1,input_dimension))
+    W1 = np.matrix(np.random.normal(0,1,input_dimension+1))
     for x in range(n-1):
-         W1 = np.r_[W1,np.matrix(np.random.normal(0,1,input_dimension))]
+         W1 = np.r_[W1,np.matrix(np.random.normal(0,1,input_dimension+1))]
          #W1 = np.r_[W1,np.matrix(np.random.normal(0,1,n+1))]
          #if you want bias in second layer
-    W2 = np.matrix(np.random.normal(0,1,n))
-    for x in range(input_dimension-1):
-        W2 = np.r_[W2,np.matrix(np.random.normal(0,1,n))]
-    
-    print("shape")
-    print(W1.shape,W2.shape)
+    W2 = np.matrix(np.random.normal(0,1,input_dimension))
+    for x in range(n):
+        W2 = np.r_[W2,np.matrix(np.random.normal(0,1,input_dimension))]
     return W1, W2
 
 
@@ -94,11 +91,14 @@ def forward_pass(data,W1,W2):
 
     sigmoid_f = np.vectorize(sigmoid)
     sigmoid_prime_f = np.vectorize(sigmoid_prime)
+    bias = [1 for i in range(1)]
+    bias = (np.matrix(bias))
     H_star = W1*data
+    H_star = np.r_[H_star,bias]
     H = sigmoid_f(H_star)
     #H = np.r_[H,bias]
     # remove this comment if you want bias in second layer
-    O_star = W2*H
+    O_star = np.transpose(W2)*H
     O = sigmoid_f(O_star)
 
 
@@ -112,8 +112,9 @@ def forward_pass(data,W1,W2):
 def backpropagation(H_star,H,O_star,O,T,sigmoid_prime_O_star,sigmoid_prime_H_star,W1,W2):
 
     delta_O = np.multiply((O-T),(sigmoid_prime_O_star))
-    placeHolder = (np.transpose(W2)*delta_O)
+    placeHolder = W2*delta_O
     delta_H = np.multiply(placeHolder,sigmoid_prime_H_star)
+    delta_H = np.delete(delta_H, 3,axis = 0)
     return delta_O,delta_H
     
     
@@ -124,23 +125,18 @@ def batch_learning(data, T, W1, W2):
     sigmoid_prime_f = np.vectorize(sigmoid_prime)
 
 def weight_update(delta_O,delta_H,X,H,etha):
-    delta_W1 = delta_H*np.transpose(X)
-    delta_W2 = delta_O*np.transpose(H)
+    delta_W1 = -delta_H*np.transpose(X)
+    delta_W2 = -delta_O*np.transpose(H)
     return delta_W1,delta_W2
     
 
-def iteration(X,T,W1,W2,etha):
-    for x in range(1,10):
-        for x in range(1,1000):
-                
-            H_star,H,O_star,O,sigmoid_prime_O_star,sigmoid_prime_H_star,H = forward_pass(X,W1,W2)
-            delta_O,delta_H = backpropagation(H_star,H,O_star,O ,T,sigmoid_prime_O_star,sigmoid_prime_H_star,W1,W2)
-            delta_W1,delta_W2 =  weight_update(delta_O,delta_H,X,H,etha)
-            W1 = W1 + delta_W1*etha
-            W2 = W2 + delta_W2*etha
-
-        #print(O)
-    return O
+def iteration(X,T,W1,W2,etha):       
+    H_star,H,O_star,O,sigmoid_prime_O_star,sigmoid_prime_H_star,H = forward_pass(X,W1,W2)
+    delta_O,delta_H = backpropagation(H_star,H,O_star,O ,T,sigmoid_prime_O_star,sigmoid_prime_H_star,W1,W2)
+    delta_W1,delta_W2 =  weight_update(delta_O,delta_H,X,H,etha)
+    W1 = W1 + delta_W1*etha
+    W2 = W2 + np.transpose(delta_W2*etha)
+    return O,W1,W2
 
 def main():
     mean1 = [0, 0]
@@ -149,15 +145,43 @@ def main():
     cov2 = [[1, 0], [0, 1]]
     etha = 0.01
     input_dimension = 8
-    X = [-1,-1,-1,-1,-1,-1,-1,1]
-    X = np.transpose(np.matrix(X))
-    data = X
-    T = X
     HiddenLayerNodes = 3
+    numberOfIterations = 1000
     W1, W2 = initialize_weights(HiddenLayerNodes,input_dimension )
+    X1 = [1,-1,-1,-1,-1,-1,-1,-1]
+    X2 = [-1,1,-1,-1,-1,-1,-1,-1]
+    X3 = [-1,-1,1,-1,-1,-1,-1,-1]
+    X4 = [-1,-1,-1,1,-1,-1,-1,-1]
+    X5 = [-1,-1,-1,-1,1,-1,-1,-1]
+    X6 = [-1,-1,-1,-1,-1,1,-1,-1]
+    X7 = [-1,-1,-1,-1,-1,-1,1,-1]
+    X8 = [-1,-1,-1,-1,-1,-1,-1,1]
+    dataList = [X1,X2,X3,X4,X5,X6,X7,X8]
+    #last one is bias
+    O = 0
+    latestX = 0
+    oldesum = 1
+    e = 0.4
+    esum = 0
+    for itera in range(numberOfIterations):
+        esum=0
+        for X in dataList:
+            T = np.transpose(np.matrix(X))
+            data = np.transpose(np.matrix(X+[1]))
+            
+            e = (O-T).sum()/16
+            esum = esum +e
+            latestX = X
+        O,W1,W2 = iteration(data,T,W1,W2,etha)
+        plt.plot([itera-1,itera],[oldesum,esum/8],c="b")
+        oldesum = esum/8
 
-    O = iteration(data,T,W1,W2,etha)
+    
+
+    print(e)
     print(O)
+    print(latestX)
+    plt.show()
     return
     
 
